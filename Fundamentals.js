3bc7d6cd6375c9d2ab149f368e11c7321f0a17f8
Vasu margana <aveenashvasu2024@gmail.com>
2026-03-02 18:58:37 +0530

    Update Fundamentals.js

--- Fundamentals.js
+++ Fundamentals.js
@@ -1,5 +1,4 @@
 Javascript is object oriented program and no distinction b/w objects.
-
 While writing functions this is syntax
 
 (function (){
@@ -10,11 +9,15 @@ While writing functions this is syntax
 })();
 
 Number is a datatype used for both integers and floats.
-
 BigInt is a datype whose range is greater than 2^53-1 and less than -(2^53-1)
-
 Null value wich points to a non-existing object
-
 Undefined is a datatype where value is not assigned
 
 A operator is unary if only one operand is there.
+We use alert it is a built in function where it haults code execution.
+Operator precedence is ()->increments->Exponentation->*->/->%->+->-
+Comma operator is used it is rare
+a = (1+2,3+4) 3+4 is only executed.
+
+
+
